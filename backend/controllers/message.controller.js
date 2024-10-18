2913d3337143280a2dbb990825b273882a2a3b25
Bratin Das <bratin4201@gmail.com>
2024-10-18 22:15:41 +0530

    Successfully implemented socket.io

--- backend/controllers/message.controller.js
+++ backend/controllers/message.controller.js
@@ -1,8 +1,9 @@
 import Conversation from "../models/conversation.model.js";
 import Message from "../models/message.model.js";
+import { getReceiverSocketId, io } from "../socket/socket.js";
 
-export const sendMessage = async(req, res) => {
-    try {
+export const sendMessage = async (req, res) => {
+	try {
 		const { message } = req.body;
 		const { id: receiverId } = req.params;
 		const senderId = req.user._id;
@@ -26,12 +27,19 @@ export const sendMessage = async(req, res) => {
 		if (newMessage) {
 			conversation.messages.push(newMessage._id);
 		}
-        //await conversation.save();
-        //await newMessage.save();
 
-        // this will run in parallel
+		// await conversation.save();
+		// await newMessage.save();
+
+		// this will run in parallel
 		await Promise.all([conversation.save(), newMessage.save()]);
 
+		// SOCKET IO FUNCTIONALITY WILL GO HERE
+		const receiverSocketId = getReceiverSocketId(receiverId);
+		if (receiverSocketId) {
+			// io.to(<socket_id>).emit() used to send events to specific client
+			io.to(receiverSocketId).emit("newMessage", newMessage);
+		}
 
 		res.status(201).json(newMessage);
 	} catch (error) {
